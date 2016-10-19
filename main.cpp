b8aa6322b9b2c3bdcc6c04e9fc2fc2025792e08f
squarepantsspongebob <mary2006lucky@sina.com>
2016-10-19 16:12:25 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <QGuiApplication>
+#include <QQuickView>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQuickView viewer;
+    viewer.setResizeMode (QQuickView::SizeRootObjectToView);
+    viewer.setSource(QUrl("qrc:///main.qml"));
+    viewer.show();
+
+    return app.exec();
+}
